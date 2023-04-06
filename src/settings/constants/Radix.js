5d5570e1c52f6c307a46cfda6e1124a5288d65af
Kibo <tomasjurman@kibo.cz>
2023-04-06 12:56:24 +0200

    v0.0.23

--- src/settings/constants/Radix.js
+++ src/settings/constants/Radix.js
@@ -6,8 +6,18 @@
 */
 export const RADIX_ID = "radix"
 
-// radius / OUTER_CIRCLE_RADIUS_RATIO
+/*
+* Outer circle radius ratio
+* @constant
+* @type {Number}
+* @default 2
+*/
 export const RADIX_OUTER_CIRCLE_RADIUS_RATIO = 2;
 
-// radius - radius/INNER_CIRCLE_RADIUS_RATIO
+/*
+* Inner circle radius ratio
+* @constant
+* @type {Number}
+* @default 2
+*/
 export const RADIX_INNER_CIRCLE_RADIUS_RATIO = 8;
