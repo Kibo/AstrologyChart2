055ca91a228f95d44ee82e10171dd266eb954cdc
Kibo <tomasjurman@kibo.cz>
2023-04-11 16:00:10 +0200

    v0.1.0

--- dist/astrochart2.js
+++ dist/astrochart2.js
@@ -2,7 +2,7 @@
  * 
  *       astrochart2
  *       A JavaScript for generating Astrology charts.
- *       Version: 0.0.1
+ *       Version: 0.1.0
  *       Author: Tom Jurman (tomasjurman@kibo.cz)
  *       Licence: GNUv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
  *
